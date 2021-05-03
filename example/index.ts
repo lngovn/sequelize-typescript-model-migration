b52ce2e34b743b3c1f4f3f9bd68b691178375c5b
lngovn <ngocongluong16@gmail.com>
2021-05-03 18:33:05 +0700

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,9 @@
-import sequelize from './sequelize';
-import { generateMigration } from '../index';
+import sequelize from "./sequelize";
+import { generateMigration } from "../index";
+import path from "path";
 
-generateMigration(sequelize, { migrationName: 'my-migration' });
+generateMigration(sequelize, {
+  migrationName: "my-migration",
+  outDir: path.join(__dirname, "./migrations"),
+  snapshotDir: path.join(__dirname, "./snapshots"),
+});
